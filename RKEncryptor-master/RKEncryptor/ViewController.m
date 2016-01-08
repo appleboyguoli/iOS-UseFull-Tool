8fe1d01b364e7cfbb7ca7172605c949e76396b38
pythonBoy <1101093069@qq.com>
2016-01-08 10:39:02 +0800

    AES算法
    AES算法的模式及补码方式为ECB/PKCS5Padding

---
+++ RKEncryptor-master/RKEncryptor/ViewController.m
@@ -0,0 +1,71 @@
+//
+//  ViewController.m
+//  RKEncryptor
+//
+//  Created by mac on 15/12/23.
+//  Copyright © 2015年 rock. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "FBEncryptorAES.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) UITextField* textField;
+@property(nonatomic, copy)  NSString *encodeStr;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    UITextField* textField = [[UITextField alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 200)/2, 200, 200, 50)];
+    textField.layer.borderColor = [UIColor grayColor].CGColor;
+    textField.layer.borderWidth = 1.0f;
+    textField.textColor = [UIColor blackColor];
+    [self.view addSubview:textField];
+    self.textField = textField;
+    
+    UIButton* ecodeBt = [UIButton buttonWithType:UIButtonTypeCustom];
+    ecodeBt.frame = CGRectMake(self.view.frame.size.width/2 - 100, textField.frame.origin.y + textField.frame.size.height + 10, 80, 50);
+    [ecodeBt setTitle:@"加密" forState:UIControlStateNormal];
+    [ecodeBt setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    ecodeBt.layer.borderColor = [UIColor grayColor].CGColor;
+    ecodeBt.layer.borderWidth = 1.0f;
+    [ecodeBt addTarget:self action:@selector(jiami:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:ecodeBt];
+    
+    UIButton* unencodeBt = [UIButton buttonWithType:UIButtonTypeCustom];
+    unencodeBt.frame = CGRectMake(self.view.frame.size.width/2+20, ecodeBt.frame.origin.y, 80, ecodeBt.frame.size.height);
+    [unencodeBt setTitle:@"解密" forState:UIControlStateNormal];
+    [unencodeBt setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+
+    unencodeBt.layer.borderColor = [UIColor grayColor].CGColor;
+    unencodeBt.layer.borderWidth = 1.0f;
+    [unencodeBt addTarget:self action:@selector(unencode:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:unencodeBt];
+    
+}
+
+- (void)jiami:(id)sender
+{
+    self.textField.text = [FBEncryptorAES encryptBase64String:@"10001sograndwap" keyString:@"1234567890123456" separateLines:YES];
+    self.encodeStr = self.textField.text;
+    
+}
+
+- (void)unencode:(id)sender
+{
+    self.textField.text = [FBEncryptorAES decryptBase64String:self.encodeStr keyString:@"1234567890123456"];
+    NSLog(@"解密：%@",self.textField.text);
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
